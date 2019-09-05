b1cd5dd64471e0c7442b75134b5f74c42f03fcaf
Chris Wendt <chrismwendt@gmail.com>
2019-09-05 13:50:20 -0600

    .

--- app.cpp
+++ app.cpp
@@ -1,7 +1,9 @@
 #include "lib.h"
+#include <stdio.h>
 
 int main() {
     // TODO support printf (no defloc on the ref)
     // printf("hello, %s\n", uppercase("world"));
-    uppercase("world");
+    printf("hello, %s\n", new Foo().uppercase("world"));
+    printf("hello, %s\n", new Bar().uppercase("world"));
 }
